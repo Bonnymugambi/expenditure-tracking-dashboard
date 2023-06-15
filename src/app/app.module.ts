6ced7e71337ea771cf43bea61b279e9fbe35e525
Bonnymugambi <bonifacemugambi46@gmail.com>
2023-06-15 10:54:24 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { PostsComponent } from './modules/posts/posts.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+  
+    HeaderComponent,
+       FooterComponent,
+       SidebarComponent,
+       PostsComponent,
+       DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
 export class AppModule { }
